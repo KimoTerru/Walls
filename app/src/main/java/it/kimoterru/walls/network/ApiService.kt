7b2886ae45d3d86d53bc47c708b9995e1318f196
4inodev <4ino.dev@gmail.com>
2021-07-30 18:42:37 +0600

    add base url

--- app/src/main/java/it/kimoterru/walls/network/ApiService.kt
+++ app/src/main/java/it/kimoterru/walls/network/ApiService.kt
@@ -5,6 +5,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
+    companion object {
+        const val BASE_URL = "https://api.unsplash.com/"
+    }
+
     @GET("/photos")
     suspend fun getLatest(@Query("client_id") clientId: String, @Query("page") page: Int): List<PhotoItem>
 }
