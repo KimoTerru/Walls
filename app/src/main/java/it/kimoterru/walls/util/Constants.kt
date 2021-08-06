6f3a664c586f1e0ed8e7c790d68b04a3118abafe
4inodev <4ino.dev@gmail.com>
2021-08-06 19:23:13 +0600

    add wallpaper colors

--- app/src/main/java/it/kimoterru/walls/util/Constants.kt
+++ app/src/main/java/it/kimoterru/walls/util/Constants.kt
@@ -1,5 +1,8 @@
 package it.kimoterru.walls.util
 
+import androidx.annotation.DrawableRes
+import it.kimoterru.walls.R
+
 class Constants {
     companion object {
         const val CLIENT_ID = "NPk5uWaTODGbpS35GoQyu7WulRGLX1jsYgn0mkVKhc4"
@@ -11,4 +14,18 @@ enum class TopicsOrder(var query: String) {
     LATEST("latest"),
     OLDEST("oldest"),
     POSITION("position") //default
+}
+
+enum class WallpaperColors(var query: String, var colorRes: Int) {
+    BLACKWHITE("black_and_white", -1),
+    BLACK("black", R.color.wp_black),
+    WHITE("white", R.color.wp_white),
+    YELLOW("yellow", R.color.wp_yellow),
+    ORANGE("orange", R.color.wp_orange),
+    RED("red", R.color.wp_red),
+    PURPLE("purple", R.color.wp_purple),
+    MAGENTA("magenta", R.color.wp_magenta),
+    GREEN("green", R.color.wp_green),
+    TEAL("teal", R.color.wp_teal),
+    BLUE("blue", R.color.wp_blue)
 }
