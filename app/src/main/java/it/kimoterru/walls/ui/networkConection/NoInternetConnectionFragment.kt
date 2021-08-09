49f3f33e6189335d29c1d99bb2ad8143abcfd308
KimoTerru <emirbajnazarov44@gmai.com>
2021-08-09 14:21:55 +0600

    Изменения: Удалил ненужный NavigationExtensions Подготовил макет который при случае должен показываться + кнопка для обновления

---
+++ app/src/main/java/it/kimoterru/walls/ui/networkConection/NoInternetConnectionFragment.kt
@@ -0,0 +1,9 @@
+package it.kimoterru.walls.ui.networkConection
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import it.kimoterru.walls.R
+
+class NoInternetConnectionFragment : Fragment(R.layout.fragment_no_internet_connection) {
+
+}
