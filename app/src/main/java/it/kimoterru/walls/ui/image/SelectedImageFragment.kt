6c072b2a313335885097ad3f4c820691569e7e1d
Eskel <emirbajnazarov44@gmail.com>
2021-11-04 22:13:55 +0600

    теперь при нажатии на BottomSheetDialog в SelectedImageFragment появляются две кнопки и они устанавливают картинки по вариантам переименовал BottomSheetLayout в BottomSheetInfo

--- app/src/main/java/it/kimoterru/walls/ui/image/SelectedImageFragment.kt
+++ app/src/main/java/it/kimoterru/walls/ui/image/SelectedImageFragment.kt
@@ -25,7 +25,8 @@ import com.bumptech.glide.request.target.Target
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 import it.kimoterru.walls.R
-import it.kimoterru.walls.databinding.BottomSheetLayoutBinding
+import it.kimoterru.walls.databinding.BottomSheetApplyToScreenBinding
+import it.kimoterru.walls.databinding.BottomSheetInfoBinding
 import it.kimoterru.walls.databinding.FragmentSelectedImageBinding
 import it.kimoterru.walls.models.photo.PhotoItem
 
@@ -96,13 +97,28 @@ class SelectedImageFragment : Fragment(R.layout.fragment_selected_image) {
         })
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "NewApi")
     private fun onClick(data: PhotoItem) {
         binding.cardBrush.setOnClickListener {
-            val wallpaperManager = WallpaperManager.getInstance(context)
-            val bitmap = (binding.selectedImage.drawable as BitmapDrawable).bitmap
-            wallpaperManager.setBitmap(bitmap)
-            Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
+            val dialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogApplyToTheme)
+            val bindingBottomSheet = BottomSheetApplyToScreenBinding.inflate(layoutInflater)
+            dialog.setContentView(bindingBottomSheet.root)
+
+            bindingBottomSheet.let {
+                it.applyToDesktop.setOnClickListener {
+                    val wallpaperManager = WallpaperManager.getInstance(context)
+                    val bitmap = (binding.selectedImage.drawable as BitmapDrawable).bitmap
+                    wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_SYSTEM)
+                    dialog.hide()
+                }
+                it.applyToLockScreen.setOnClickListener {
+                    val wallpaperManager = WallpaperManager.getInstance(context)
+                    val bitmap = (binding.selectedImage.drawable as BitmapDrawable).bitmap
+                    wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_LOCK)
+                    dialog.hide()
+                }
+            }
+            dialog.show()
         }
         binding.cardDown.setOnClickListener {
             Toast.makeText(context, "Start download...", Toast.LENGTH_LONG).show()
@@ -122,7 +138,7 @@ class SelectedImageFragment : Fragment(R.layout.fragment_selected_image) {
         }
         binding.cardInfo.setOnClickListener {
             val dialog = BottomSheetDialog(requireContext(), R.style.AppBottomSheetDialogTheme)
-            val bindingBottomSheet = BottomSheetLayoutBinding.inflate(layoutInflater)
+            val bindingBottomSheet = BottomSheetInfoBinding.inflate(layoutInflater)
             dialog.setContentView(bindingBottomSheet.root)
 
             bindingBottomSheet.let {
